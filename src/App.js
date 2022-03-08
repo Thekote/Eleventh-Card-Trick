e6fdde4773a94c6314e0b403d484d660a1f7ab4e
Guilherme Monteiro <dkglima@gmail.com>
2022-03-08 18:28:15 -0300

    styles: add ITCSS pattern and add some css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import CardsList from "./components/CardsList";
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <h1>Eleventh Card Trick</h1>
       <CardsList />
     </div>
